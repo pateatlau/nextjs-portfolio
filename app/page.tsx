445cb013dc3803aa01cc36ca79db0e167ebed077
Patea Tlau <pateatlau@gmail.com>
2025-03-08 09:58:21 +0530

    Removed syntax error

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,9 @@
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full"><h1>Hello portfolio</h1>
-        <
+      <div className="max-w-7xl w-full">
+        <h1>Hello portfolio</h1>
+
         {/* <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
