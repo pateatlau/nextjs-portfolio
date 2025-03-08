8b5fa8116504dd7f880ea8c1ddac3a0d42c3c845
Patea Tlau <pateatlau@gmail.com>
2025-03-08 10:30:12 +0530

    Nav uncommented

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-// import { navItems } from '@/data';
+import { navItems } from '@/data';
 
-// import { FloatingNav } from '@/components/ui/FloatingNavbar';
+import { FloatingNav } from '@/components/ui/FloatingNavbar';
 import Hero from '@/components/Hero';
 // import Grid from '@/components/Grid';
 // import RecentProjects from '@/components/RecentProjects';
@@ -15,6 +15,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         {/* <Grid /> */}
         {/* <RecentProjects /> */}
