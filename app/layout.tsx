85c3614574b4f7ae1e416a0540b05ed75d24c990
Patea Tlau <pateatlau@gmail.com>
2025-03-08 12:32:05 +0530

    App icon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <link
           rel="icon"
-          href="/jsm-logo.png"
+          href="/jsm-logo.jpg"
           sizes="any"
         />
       </head>
