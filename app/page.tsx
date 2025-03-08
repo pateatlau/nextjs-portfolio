8ef09e5fa19166a814927cb4b3f9419e6a5ebd1c
Patea Tlau <pateatlau@gmail.com>
2025-03-08 10:27:34 +0530

    Grid section commented back

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 
 // import { FloatingNav } from '@/components/ui/FloatingNavbar';
 import Hero from '@/components/Hero';
-import Grid from '@/components/Grid';
+// import Grid from '@/components/Grid';
 // import RecentProjects from '@/components/RecentProjects';
 // import Footer from '@/components/Footer';
 // import Clients from '@/components/Clients';
@@ -16,7 +16,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <Grid />
+        {/* <Grid /> */}
         {/* <RecentProjects /> */}
 
         {/* <FloatingNav navItems={navItems} />
