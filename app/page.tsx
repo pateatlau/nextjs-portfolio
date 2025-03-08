03dc48f204d9f36677a8f1d02419c0f5608fc35c
Patea Tlau <pateatlau@gmail.com>
2025-03-08 10:07:02 +0530

    Hero uncommented

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 // import { navItems } from '@/data';
 
-// import Hero from '@/components/Hero';
+import Hero from '@/components/Hero';
 // import Grid from '@/components/Grid';
 // import Footer from '@/components/Footer';
 // import Clients from '@/components/Clients';
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello portfolio</h1>
+        <Hero />
 
         {/* <FloatingNav navItems={navItems} />
         <Hero />
